e1a935c0e667b5afa904dc2a960a4466f74915c4
Henrique Malheiros <henriqemalheiros@gmail.com>
2021-02-03 18:09:22 -0300

    feat: adds basic head configuration in `nuxt.config.js`

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,4 +3,19 @@ export default {
     '@nuxtjs/tailwindcss',
     '~/modules/scraper.js',
   ],
+  head: {
+    title: 'Star Wars explorer',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+    ],
+  },
 };
