e0fa87a5de0743fdfe13d378b230564888a3f7af
Henrique Malheiros <henriqemalheiros@gmail.com>
2021-02-04 20:18:27 -0300

    feat(components): creates `InfoData`

---
+++ components/info-data/InfoData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex items-center">
+    <div class="px-4 py-3 text-xs text-gray-400 uppercase">
+      {{ label }}
+    </div>
+    <div class="pr-4 py-3">
+      <slot
+        v-if="$scopedSlots.default && $scopedSlots.default().length"
+      />
+      <span
+        v-else
+        class="text-sm"
+      >
+        {{ value }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoData',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      default: undefined,
+    },
+  },
+};
+</script>
