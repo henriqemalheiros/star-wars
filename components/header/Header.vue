8e37e4a30d03bcbb3553cc5791a545d7f3f29dee
Henrique Malheiros <henriqemalheiros@gmail.com>
2021-02-03 22:07:00 -0300

    fix(components): small `Header` tweak

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="relative py-8 sm:py-16">
+    <div class="absolute z-0 w-full max-w-screen-md top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <HeaderStars/>
+    </div>
     <div class="relative z-10">
       <Logo class="block w-1/2 sm:w-1/3 h-auto mx-auto"/>
-    </div>
-    <div class="w-full max-w-screen-md absolute z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <HeaderStars/>
     </div>
   </div>
 </template>
