c43b818fbdbabcb691b2ed4b3adc6811ea1d84ba
Henrique Malheiros <henriqemalheiros@gmail.com>
2021-02-04 22:12:06 -0300

    feat(components): visual tweak on `InfoData`

--- components/info-data/InfoData.vue
+++ components/info-data/InfoData.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex items-center">
-    <div class="px-4 py-3 text-xs text-gray-400 uppercase">
+  <div class="flex items-start">
+    <div class="h-12 flex items-center px-4 py-3 text-xs text-gray-400 uppercase">
       {{ label }}
     </div>
-    <div class="pr-4 py-3">
+    <div class="m-h-12 pr-4 py-3">
       <slot
         v-if="$scopedSlots.default && $scopedSlots.default().length"
       />
